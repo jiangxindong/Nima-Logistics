0d2c38a2995f4bec9546def25a2ee5a79028ae79
jieao <3176340021@qq.com>
2018-12-27 19:31:50 +0800

    LoginServlet类完成

--- src/main/java/pers/geolo/LogisticsAssistant/servlet/LoginServlet.java
+++ src/main/java/pers/geolo/LogisticsAssistant/servlet/LoginServlet.java
@@ -1,7 +1,8 @@
 package pers.geolo.LogisticsAssistant.servlet;
 
 import pers.geolo.LogisticsAssistant.entity.User;
-import pers.geolo.LogisticsAssistant.utils.BeanUtils;
+import pers.geolo.LogisticsAssistant.service.UserService;
+import pers.geolo.LogisticsAssistant.utils.MyBeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,14 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author 桀骜
+ */
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        User user = (User) BeanUtils.getBean(request);
-//        boolean result = UserService.getInstance().login(user);
-        boolean result = true;
+        User user = MyBeanUtils.getBean(request, User.class);
+        boolean result = UserService.getInstance().login(user);
         if (result) { // 登录成功
             // 登录信息存储至session
             request.getSession().setAttribute("user", user);
@@ -31,7 +34,8 @@ public class LoginServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") != null) { // 如果存在会话信息，则自动登录
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null) { // 如果存在会话信息，则自动登录
             // 重定向至主页面
             response.sendRedirect(request.getContextPath() + "/index");
         } else {
