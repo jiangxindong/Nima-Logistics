145a89f251ced69e7ef3014fb6fb66f7747a30d5
jieao <3176340021@qq.com>
2018-12-27 19:44:03 +0800

    ProfileServlet类完成

--- src/main/java/pers/geolo/LogisticsAssistant/servlet/ProfileServlet.java
+++ src/main/java/pers/geolo/LogisticsAssistant/servlet/ProfileServlet.java
@@ -1,8 +1,9 @@
 package pers.geolo.LogisticsAssistant.servlet;
 
+import org.apache.commons.beanutils.BeanUtils;
 import pers.geolo.LogisticsAssistant.entity.User;
 import pers.geolo.LogisticsAssistant.service.UserService;
-import pers.geolo.LogisticsAssistant.utils.BeanUtils;
+import pers.geolo.LogisticsAssistant.utils.MyBeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,26 +12,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author 桀骜
+ */
 @WebServlet(name = "ProfileServlet", urlPatterns = "/profile")
 public class ProfileServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = (User) BeanUtils.getBean(request);
-        boolean result = UserService.getInstance().update(user);
-        if (result) { // 修改成功
-            // 登录信息存储至session
-            request.getSession().setAttribute("user", user);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        User oldUser = (User) request.getSession().getAttribute("user");
+        User newUser = MyBeanUtils.getBean(request, User.class);
+        if (oldUser.getUsername().equals(newUser.getUsername())) { // 和会话中的用户名匹配
+            UserService.getInstance().update(newUser);
+            // 更新后的信息存储至session
+            request.getSession().setAttribute("user", newUser);
             // 跳转至修改成功页面
             response.sendRedirect(request.getContextPath() + "/updateSuccess");
-        } else { // 登录失败
+        } else { // 修改失败
             // 跳转至修改失败页面
             request.getRequestDispatcher("/updateFailed.jsp").forward(request, response);
         }
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/myProfile.jsp").forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+        } else {
+            request.getRequestDispatcher("/myProfile.jsp").forward(request, response);
+        }
     }
 }
