158f2195c4b7fcc0f77525d06a6110542c8efafc
jieao <3176340021@qq.com>
2018-12-27 19:50:34 +0800

    RegisterServlet类完成

--- src/main/java/pers/geolo/LogisticsAssistant/servlet/RegisterServlet.java
+++ src/main/java/pers/geolo/LogisticsAssistant/servlet/RegisterServlet.java
@@ -1,9 +1,9 @@
 package pers.geolo.LogisticsAssistant.servlet;
 
 import pers.geolo.LogisticsAssistant.entity.User;
-import pers.geolo.LogisticsAssistant.utils.BeanUtils;
+import pers.geolo.LogisticsAssistant.service.UserService;
+import pers.geolo.LogisticsAssistant.utils.MyBeanUtils;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,30 +11,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @author 桀骜
+ */
 @WebServlet(name = "RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        User user = (User) BeanUtils.getBean(request);
-//        boolean result = UserService.getInstance().register(user);
-        boolean result = true;
+        User user = MyBeanUtils.getBean(request, User.class);
+        boolean result = UserService.getInstance().register(user);
         if (result) { // 注册成功
-            System.out.println(user.getUsername());
             // 登录信息存储至session
             request.getSession().setAttribute("user", user);
-            // 跳转至registerSuccessful页面
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/registerSuccess.jsp");
-            dispatcher.forward(request, response);
-        } else {
-            // 注册失败
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/registerFailed.jsp");
-            dispatcher.forward(request, response);
+            // 转发至registerSuccess页面
+            request.getRequestDispatcher("/registerSuccess.jsp").forward(request, response);
+        } else { // 注册失败
+            // 转发至registerFailed页面
+            request.getRequestDispatcher("/registerFailed.jsp").forward(request, response);
         }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 跳转至注册界面
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 转发至注册界面
         request.getRequestDispatcher("/register.jsp").forward(request, response);
     }
 }
